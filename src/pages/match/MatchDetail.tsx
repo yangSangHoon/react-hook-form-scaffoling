9008eaa8f9b402594dcb9637161965cffca3aa89
양상훈 <dazzybana@gmail.com>
2021-10-23 14:33:23 +0900

    2. validation & error

--- src/pages/match/MatchDetail.tsx
+++ src/pages/match/MatchDetail.tsx
@@ -1,6 +1,6 @@
 import { RouteComponentProps } from 'react-router';
-import MatchDetailStyle from './MatchDetailStyle';
 import { useForm } from 'react-hook-form';
+import MatchDetailStyle from './MatchDetailStyle';
 
 interface Params {
     id: string;
@@ -28,58 +28,61 @@ const MatchDetail: React.VFC<RouteComponentProps<Params>> = ({ match }) => {
     const name = watch('name');
     console.log(name);
 
+    console.log('errors', errors);
+
     return (
-        <article css={MatchDetailStyle}>
+        <MatchDetailStyle>
             <h1>기본 정보</h1>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <table>
                     <tbody>
                         <tr>
-                            <td>이름</td>
                             <td>
-                                <input type="text" {...register('name')}></input>
+                                <label>이름</label>
+                                <div>
+                                    <input type="text" {...register('name', { required: true, maxLength: 4 })}></input>
+                                    {errors.name?.type === 'maxLength' && <div className="error">4글자 이하로 입력해 주세요.</div>}
+                                    {errors.name?.type === 'required' && <div className="error">이름은 필수 값입니다.</div>}
+                                </div>
                             </td>
                         </tr>
                         <tr>
-                            <td>계약 ID</td>
                             <td>
-                                <input type="text" {...register('contractId', { required: true })}></input>
-                                {errors.contractId && <div className="error">계약 ID는 필수 값입니다.</div>}
+                                <label>계약 ID</label>
+                                <div>
+                                    <input type="text" {...register('contractId', { required: true, pattern: /^[A-Za-z]+$/i })}></input>
+                                    {errors.contractId?.type === 'pattern' && <div className="error">영문만 입력 가능합니다.</div>}
+                                    {errors.contractId?.type === 'required' && <div className="error">계약 ID는 필수 값입니다.</div>}
+                                </div>
                             </td>
                         </tr>
                         <tr>
-                            <td>성별</td>
                             <td>
-                                <select {...register('gender')}>
-                                    <option value="female">female</option>
-                                    <option value="male">male</option>
-                                    <option value="other">other</option>
-                                </select>
+                                <label>성별</label>
+                                <div>
+                                    <select {...register('gender')}>
+                                        <option value="female">female</option>
+                                        <option value="male">male</option>
+                                        <option value="other">other</option>
+                                    </select>
+                                </div>
                             </td>
                         </tr>
                         <tr>
-                            <td>프리랜서 명</td>
                             <td>
-                                <input type="text"></input>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>프로젝트 명</td>
-                            <td>
-                                <input type="text"></input>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>직군 / 직무</td>
-                            <td>
-                                <input type="text"></input>
+                                <label>나이</label>
+                                <div>
+                                    <input type="number" {...register('age', { min: 18, max: 999 })} />
+                                    {errors.age?.type === 'min' && <div className="error">18이상으로 입력해 주세요.</div>}
+                                    {errors.age?.type === 'max' && <div className="error">999세 이하로 입력해 주세요</div>}
+                                </div>
                             </td>
                         </tr>
                     </tbody>
                 </table>
                 <button type="submit">저장</button>
             </form>
-        </article>
+        </MatchDetailStyle>
     );
 };
 
--- src/pages/match/MatchDetailStyle.ts
+++ src/pages/match/MatchDetailStyle.ts
@@ -1,12 +1,44 @@
-import { css, jsx } from '@emotion/react';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
 
-const MatchDetailStyle = css`
+const MatchDetailStyle = styled.div`
     border: 1px solid gray;
     padding: 20px;
+    max-width: 500px;
 
-    background-color: gray;
-    & .error {
+    .error {
         color: red;
+    }
+
+    table {
+        width: 100%;
+    }
+
+    td {
+        display: flex;
+        align-items: center;
+        label {
+            display: inline-block;
+            width: 80px;
+        }
+        div {
+            width: 100%;
+        }
+        input,
+        select {
+            width: 100%;
+            height: 30px;
+            padding: 0;
+        }
+    }
+
+    button {
+        width: 100px;
+        height: 50px;
+        text-align: center;
+        margin: 0 auto;
+        display: block;
+        margin-top: 10px;
     }
 `;
 
