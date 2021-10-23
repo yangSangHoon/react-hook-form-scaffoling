0eb4e4eda746a9f9f6c3509628c9f75f5002858c
양상훈 <dazzybana@gmail.com>
2021-10-23 13:48:25 +0900

    1. basic

--- src/pages/match/MatchDetail.tsx
+++ src/pages/match/MatchDetail.tsx
@@ -1,54 +1,84 @@
 import { RouteComponentProps } from 'react-router';
 import MatchDetailStyle from './MatchDetailStyle';
+import { useForm } from 'react-hook-form';
 
 interface Params {
     id: string;
 }
 
+interface DefaultInfo {
+    id: string;
+    contractId: string;
+    gender: boolean;
+}
+
 const MatchDetail: React.VFC<RouteComponentProps<Params>> = ({ match }) => {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors }
+    } = useForm();
+
+    const onSubmit = (data: DefaultInfo) => console.log(data);
+
+    /*
+        name  마다 리랜더링
+    */
+    const name = watch('name');
+    console.log(name);
+
     return (
         <article css={MatchDetailStyle}>
             <h1>기본 정보</h1>
-            <table>
-                <tbody>
-                    <tr>
-                        <td>분류</td>
-                        <td>
-                            <input type="text"></input>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>계약 ID</td>
-                        <td>
-                            <input type="text"></input>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>클라이언트 명</td>
-                        <td>
-                            <input type="text"></input>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>프리랜서 명</td>
-                        <td>
-                            <input type="text"></input>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>프로젝트 명</td>
-                        <td>
-                            <input type="text"></input>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>직군 / 직무</td>
-                        <td>
-                            <input type="text"></input>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>이름</td>
+                            <td>
+                                <input type="text" {...register('name')}></input>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>계약 ID</td>
+                            <td>
+                                <input type="text" {...register('contractId', { required: true })}></input>
+                                {errors.contractId && <div className="error">계약 ID는 필수 값입니다.</div>}
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>성별</td>
+                            <td>
+                                <select {...register('gender')}>
+                                    <option value="female">female</option>
+                                    <option value="male">male</option>
+                                    <option value="other">other</option>
+                                </select>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>프리랜서 명</td>
+                            <td>
+                                <input type="text"></input>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>프로젝트 명</td>
+                            <td>
+                                <input type="text"></input>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>직군 / 직무</td>
+                            <td>
+                                <input type="text"></input>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+                <button type="submit">저장</button>
+            </form>
         </article>
     );
 };
--- src/pages/match/MatchDetailStyle.ts
+++ src/pages/match/MatchDetailStyle.ts
@@ -1,8 +1,13 @@
-import { css } from '@emotion/react';
+import { css, jsx } from '@emotion/react';
 
 const MatchDetailStyle = css`
     border: 1px solid gray;
     padding: 20px;
+
+    background-color: gray;
+    & .error {
+        color: red;
+    }
 `;
 
 export default MatchDetailStyle;
